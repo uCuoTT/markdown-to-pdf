c00e4f09e3b79e3e270ec73601b561ff0b976f41
uCuoTT <zeollamarco@gmail.com>
2026-02-12 14:20:00 +0100

    Fix: Aggiornato test automatico con il nuovo titolo della pagina

--- app/test_app.py
+++ app/test_app.py
@@ -3,22 +3,15 @@ from app import app
 
 class BasicTests(unittest.TestCase):
 
-    # 1. Preparazione del test
     def setUp(self):
-        # Mettiamo l'app in modalità test
         app.testing = True
         self.app = app.test_client()
 
-    # 2. Controlla se la Homepage risponde
     def test_home_page_loads(self):
-        # Finge di andare su "/" (la home)
         response = self.app.get('/')
-        
-        # Verifica che il codice di risposta sia 200 
         self.assertEqual(response.status_code, 200)
         
-        # Verifica che nella pagina ci sia la scritta giusta
-        self.assertIn(b"Markdown Converter", response.data)
+        self.assertIn(b"Cloud Converter", response.data)
 
 if __name__ == "__main__":
     unittest.main()
